0c2df6de0d87c711a74d092ad1418c0b5f690533
Martin LindstrÃ¶m <martin.lindstrom@litsec.se>
2020-10-22 16:59:05 +0200

    IS-24 Fixed state for REST

---
+++ integration-impl/src/main/java/se/idsec/signservice/integration/state/impl/EncodedSignatureState.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2019-2020 IDsec Solutions AB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package se.idsec.signservice.integration.state.impl;
+
+import java.io.Serializable;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import se.idsec.signservice.integration.core.SignatureState;
+
+/**
+ * Encoded signature state.
+ * 
+ * @author Martin Lindström (martin@idsec.se)
+ * @author Stefan Santesson (stefan@idsec.se)
+ */
+@JsonInclude(Include.NON_NULL)
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class EncodedSignatureState implements SignatureState {
+
+  /** For serializing. */
+  private static final long serialVersionUID = 7403001397690504390L;
+
+  /**
+   * The state ID.
+   * 
+   * @param id
+   *          the state ID
+   */
+  @Setter
+  private String id;
+  
+  /**
+   * The compressed session state.
+   * 
+   * @param state
+   *          the compressed session state
+   */
+  @Setter
+  private EncodedSignatureSessionState state;
+
+  /** {@inheritDoc} */
+  @Override
+  public String getId() {
+    return this.id;
+  }
+
+  /** {@inheritDoc} */
+  @Override
+  public Serializable getState() {
+    return this.state;
+  }
+
+}
