1d623e7db20c73518927feba251b05fb0ec0f795
Martin Lindström <martin.lindstrom@litsec.se>
2023-10-22 18:06:24 +0200

    2.0.0

--- docs/javadoc/xml/package-search-index.js
+++ docs/javadoc/xml/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"se.idsec.signservice.integration.document.xml"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"se.idsec.signservice.integration.document.xml"}];updateSearchResults();
